73fd7702c0018f460c3ac07a9a93aa6d1b733908
David Moreno García <david.mogar@gmail.com>
2015-10-29 23:55:18 +0100

    Add facility constructor to function type

--- src/com/davidmogar/njc/ast/types/FunctionType.java
+++ src/com/davidmogar/njc/ast/types/FunctionType.java
@@ -1,15 +1,21 @@
 package com.davidmogar.njc.ast.types;
 
 import com.davidmogar.njc.ast.AbstractAstNode;
+import com.davidmogar.njc.ast.statements.definitions.VariableDefinition;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FunctionType extends AbstractAstNode implements Type {
 
-    public List<Type> parameters;
+    public List<VariableDefinition> parameters;
     public Type returnType;
 
-    public FunctionType(int line, int column, List<Type> parameters, Type returnType) {
+    public FunctionType(int line, int column, Type returnType) {
+        this(line, column, new ArrayList<>(), returnType);
+    }
+
+    public FunctionType(int line, int column, List<VariableDefinition> parameters, Type returnType) {
         super(line, column);
         this.parameters = parameters;
         this.returnType = returnType;
