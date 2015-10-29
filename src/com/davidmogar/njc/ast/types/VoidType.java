c79305aa2776421c63c6ab540c5be31d8b263a76
David Moreno García <david.mogar@gmail.com>
2015-10-29 23:59:23 +0100

    Add void type

---
+++ src/com/davidmogar/njc/ast/types/VoidType.java
@@ -0,0 +1,21 @@
+package com.davidmogar.njc.ast.types;
+
+import com.davidmogar.njc.ast.AbstractAstNode;
+
+public class VoidType extends AbstractAstNode implements Type {
+
+    private static VoidType instance;
+
+    private VoidType(int line, int column) {
+        super(line, column);
+    }
+
+    public static VoidType getInstance(int line, int column) {
+        if (instance == null) {
+            instance = new VoidType(line, column);
+        }
+
+        return instance;
+    }
+    
+}
