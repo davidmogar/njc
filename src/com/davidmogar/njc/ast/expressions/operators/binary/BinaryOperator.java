ed40e08a41981f0bf06538989626034948028d9f
David Moreno García <david.mogar@gmail.com>
2015-10-29 21:51:02 +0100

    Add operator field

--- src/com/davidmogar/njc/ast/expressions/operators/binary/BinaryOperator.java
+++ src/com/davidmogar/njc/ast/expressions/operators/binary/BinaryOperator.java
@@ -7,11 +7,13 @@ public abstract class BinaryOperator extends AbstractAstNode implements Expressi
 
     public Expression leftValue;
     public Expression rightValue;
+    public String operator;
 
-    public BinaryOperator(int line, int column, Expression leftValue, Expression rightValue) {
+    public BinaryOperator(int line, int column, Expression leftValue, Expression rightValue, String operator) {
         super(line, column);
         this.leftValue = leftValue;
         this.rightValue = rightValue;
+        this.operator = operator;
     }
 
 }
