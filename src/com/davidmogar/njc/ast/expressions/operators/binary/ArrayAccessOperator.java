69a9fb782532fdda19e612f045338a2e10508025
David Moreno García <david.mogar@gmail.com>
2015-10-29 22:50:30 +0100

    Modify ArrayAccessOperator to not ask for an operator

--- src/com/davidmogar/njc/ast/expressions/operators/binary/ArrayAccessOperator.java
+++ src/com/davidmogar/njc/ast/expressions/operators/binary/ArrayAccessOperator.java
@@ -4,8 +4,8 @@ import com.davidmogar.njc.ast.expressions.Expression;
 
 public class ArrayAccessOperator extends BinaryOperator {
 
-    public ArrayAccessOperator(int line, int column, Expression leftExpression, Expression rightExpression, String operator) {
-        super(line, column, leftExpression, rightExpression, operator);
+    public ArrayAccessOperator(int line, int column, Expression leftExpression, Expression rightExpression) {
+        super(line, column, leftExpression, rightExpression, "[]");
     }
 
 }
