30f676ef353b222d064fecc3c7a14174165fa901
David Moreno García <david.mogar@gmail.com>
2015-10-29 21:42:15 +0100

    Add invocation statement

---
+++ src/com/davidmogar/njc/ast/statements/InvocationStatement.java
@@ -0,0 +1,20 @@
+package com.davidmogar.njc.ast.statements;
+
+import com.davidmogar.njc.ast.AbstractAstNode;
+import com.davidmogar.njc.ast.expressions.Expression;
+import com.davidmogar.njc.ast.expressions.Variable;
+
+import java.util.List;
+
+public class InvocationStatement extends AbstractAstNode implements Statement {
+
+    public List<Expression> arguments;
+    public Variable variable;
+
+    public InvocationStatement(int line, int column, Variable variable, List<Expression> arguments) {
+        super(line, column);
+        this.variable = variable;
+        this.arguments = arguments;
+    }
+
+}
