f47b167bc87b28c122b79245cb5c4d33a39cc321
David Moreno García <david.mogar@gmail.com>
2015-10-29 21:54:20 +0100

    Fix old binary operators
    Arithmetic and logical operators now extend from BinaryOperator.

--- src/com/davidmogar/njc/ast/expressions/operators/binary/LogicalOperator.java
+++ src/com/davidmogar/njc/ast/expressions/operators/binary/LogicalOperator.java
@@ -2,9 +2,9 @@ package com.davidmogar.njc.ast.expressions.operators.binary;
 
 import com.davidmogar.njc.ast.expressions.Expression;
 
-public class ArithmeticOperator extends BinaryOperator {
+public class LogicalOperator extends BinaryOperator {
 
-    public ArithmeticOperator(int line, int column, Expression leftValue, Expression rightValue, String operator) {
+    public LogicalOperator(int line, int column, Expression leftValue, Expression rightValue, String operator) {
         super(line, column, leftValue, rightValue, operator);
     }
 
