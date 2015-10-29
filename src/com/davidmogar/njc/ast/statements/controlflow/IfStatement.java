56ca5d0772c52eda270c9efdfa9bc8e70751f118
David Moreno García <david.mogar@gmail.com>
2015-10-29 21:38:36 +0100

    Add control flow statements

---
+++ src/com/davidmogar/njc/ast/statements/controlflow/IfStatement.java
@@ -0,0 +1,21 @@
+package com.davidmogar.njc.ast.statements.controlflow;
+
+import com.davidmogar.njc.ast.AbstractAstNode;
+import com.davidmogar.njc.ast.expressions.Expression;
+import com.davidmogar.njc.ast.statements.Block;
+import com.davidmogar.njc.ast.statements.Statement;
+
+public class IfStatement extends AbstractAstNode implements Statement {
+
+    public Block elseBlock;
+    public Block ifBlock;
+    public Expression condition;
+
+    public IfStatement(int line, int column, Expression condition, Block ifBlock, Block elseBlock) {
+        super(line, column);
+        this.condition = condition;
+        this.ifBlock = ifBlock;
+        this.elseBlock = elseBlock;
+    }
+
+}
---
+++ src/com/davidmogar/njc/ast/statements/controlflow/WhileStatement.java
@@ -0,0 +1,19 @@
+package com.davidmogar.njc.ast.statements.controlflow;
+
+import com.davidmogar.njc.ast.AbstractAstNode;
+import com.davidmogar.njc.ast.expressions.Expression;
+import com.davidmogar.njc.ast.statements.Block;
+import com.davidmogar.njc.ast.statements.Statement;
+
+public class WhileStatement extends AbstractAstNode implements Statement {
+
+    public Block block;
+    public Expression condition;
+
+    public WhileStatement(int line, int column, Expression condition, Block block) {
+        super(line, column);
+        this.condition = condition;
+        this.block = block;
+    }
+
+}
